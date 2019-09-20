fdfeece2c19993edf6514db894b82d04303d1749
Jhon Fredy <jhonfs_16@hotmail.com>
2019-09-19 19:58:08 -0500

    Se añade el 5to saludo al archivo

--- saludar.js
+++ saludar.js
@@ -9,35 +9,30 @@ stdin.addListener("data", function(d) {
     var saludo1 = "Español: ¡Hola! " + dato + ".";
     var saludo2 = "English: ¡Hello! " + dato + ".";
     var saludo3 = "Deutsch: ¡Hallo! " + dato + ".";
-    //var saludo4 = "Español: ¡ 안녕 ! " + dato + ".";
-    //var saludo5 = "Español: ¡Hola! " + dato + ".";
+    var saludo4 = "Koreano: ¡ 안녕 ! " + dato + ".";
+    var saludo5 = "Francés: ¡ Salut! " + dato + ".";
 
     console.log("");
     console.log("");
     console.log("Tus saludos son:");
-    console.log("");
     console.log(saludo1);
     console.log(saludo2);
     console.log(saludo3);
-    
-
-    // if(dato == 'salir'){
-    //     return process.stdin.destroy();
-    // }
+    console.log(saludo4);
+    console.log(saludo5);
 
     fs.writeFile(
-        'Saludo' + dato + '.txt', 
+        'Saludos de ' + dato + '.txt', 
         'Aquí estan tus saludos '+ dato +': \n \n' +
         saludo1 + "\n" + 
         saludo2 + "\n" +
-        saludo3 + "\n",
+        saludo3 + "\n" +
+        saludo4 + "\n" +
+        saludo5 + "\n",
         (err) => {
             if (err) 
                 throw err;
         console.log('Se guardó un archivo de texto con tus saludos.');
       });
     
-    
 });
-
-
